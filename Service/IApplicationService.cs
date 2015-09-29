a81a91923029c62032c0e2d12352b8cca22d32a2
minsterdead <minsterdead@gmail.com>
2015-09-29 09:35:05 -0400

    Commit Initial

---
+++ Service/IApplicationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEM_C_E.Service
+{
+    public interface IApplicationService
+    {
+        void ChangeView<T>(T view);
+    }
+}
